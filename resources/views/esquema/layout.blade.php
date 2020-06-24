d6198a0bd0290f17aa791045b01d6c8483f196e3
12homero12 <homero_15_soccer@hotmail.com>
2020-06-24 15:08:47 -0400

    Commit modificado de esquema principal

--- resources/views/esquema/layout.blade.php
+++ resources/views/esquema/layout.blade.php
@@ -9,27 +9,29 @@
         <link rel="stylesheet" href="{{asset('css/all.css')}}">
         {{-- css principal --}}
         <link rel="stylesheet" href="{{asset('css/principal_3.css')}}">
-        <title>principal</title>
+        <title>@yield('titulo','Inventario')| Sistemas HomeroSIS</title>
     </head>
     <body>
         {{--  header  --}}
         @include('esquema.header')
         {{-- header fin --}}
-        <div class="container-fluid contenido_secundario">
-            <div class="row">
+        <div class="container-fluid contenido_secundario_primario">
+            <div class="row contenido_secundario">
                 {{-- menu vertical --}}
                 @include('esquema.aside')
                 {{-- menu vertical fin --}}
                 <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10 contenedor_principal">
                     <div class="header_cont_ppal">encabezado</div>
                     <div class="informacion_cont_ppal">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos voluptatem dolore odio porro fugiat tempore modi debitis vero quidem deserunt quasi praesentium, dolor earum sed itaque ea consequuntur inventore commodi.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veniam dolorem beatae eaque illo! Accusantium iure fuga odit nihil voluptate, facere, ipsa officia saepe nostrum nesciunt pariatur. In, ab deserunt.
                     </div>             
                 </div>
             </div>
